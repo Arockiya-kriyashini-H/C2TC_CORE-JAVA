5905f694aa1dbf0f71a075b7a2156180cdb89ee3
akriy <akriy@LAPTOP-CS49FEEG>
2024-10-25 15:09:37 +0530

    firstcommit

---
+++ C2TC_AROCKIYA/src/com/tns/ifet/day1/FirstClass.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.day1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		//program to demonstrate the identifiers
+	         int $number=20;
+	         System.out.println("value of the number variable is:"+$number);
+
+	}
+	
+
+}
+
+
+
